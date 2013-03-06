f68c954f135aa62da1537a83190b0d40d0660713
Vlad <vlad@MacBook-Pro-Vladislav.local>
2013-03-06 15:48:48 +0700

    Added Template.

---
+++ Game/AppDelegate.h
@@ -0,0 +1,13 @@
+
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Game/HeroContactListener.h
@@ -0,0 +1,20 @@
+
+
+#import "Box2D.h"
+
+#define kMaxAngleDiff 2.4f // in radians
+
+@class Hero;
+
+class HeroContactListener : public b2ContactListener {
+public:
+	Hero *_hero;
+	
+	HeroContactListener(Hero* hero);
+	~HeroContactListener();
+	
+	void BeginContact(b2Contact* contact);
+	void EndContact(b2Contact* contact);
+	void PreSolve(b2Contact* contact, const b2Manifold* oldManifold);
+	void PostSolve(b2Contact* contact, const b2ContactImpulse* impulse);
+};
